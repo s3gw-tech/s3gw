56a20d65571b1403606169ef886eb8fc4a186fb6
Volker Theile <vtheile@suse.com>
2022-05-20 09:54:51 +0200

    Strip unnecessary data from radosgw binary.
    References:
    - https://github.com/Debian/debhelper/blob/master/dh_strip
    - https://www.thegeekstuff.com/2012/09/strip-command-examples/
    
    Fixes: https://github.com/aquarist-labs/s3gw-core/issues/54
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- build/build-radosgw.sh
+++ build/build-radosgw.sh
@@ -2,9 +2,16 @@
 
 set -e
 
+# https://cmake.org/cmake/help/latest/variable/CMAKE_BUILD_TYPE.html
+# Release: Your typical release build with no debugging information and full optimization.
+# MinSizeRel: A special Release build optimized for size rather than speed.
+# RelWithDebInfo: Same as Release, but with debugging information.
+# Debug: Usually a classic debug build including debugging information, no optimization etc.
+CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-"Debug"}
+
 CEPH_DIR=$(realpath ${CEPH_DIR:-"/srv/ceph"})
 CEPH_CMAKE_ARGS="-DENABLE_GIT_VERSION=ON -DWITH_PYTHON3=3 -DWITH_CCACHE=ON ${CEPH_CMAKE_ARGS}"
-CEPH_CMAKE_ARGS="-DWITH_TESTS=OFF -DCMAKE_BUILD_TYPE=Release ${CEPH_CMAKE_ARGS}"
+CEPH_CMAKE_ARGS="-DWITH_TESTS=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${CEPH_CMAKE_ARGS}"
 CEPH_CMAKE_ARGS="-DWITH_RADOSGW_AMQP_ENDPOINT=OFF -DWITH_RADOSGW_KAFKA_ENDPOINT=OFF ${CEPH_CMAKE_ARGS}"
 CEPH_CMAKE_ARGS="-DWITH_RADOSGW_SELECT_PARQUET=OFF -DWITH_RADOSGW_MOTR=OFF ${CEPH_CMAKE_ARGS}"
 CEPH_CMAKE_ARGS="-DWITH_RADOSGW_DBSTORE=ON -DWITH_RADOSGW_LUA_PACKAGES=OFF ${CEPH_CMAKE_ARGS}"
@@ -16,6 +23,7 @@ build_radosgw() {
   echo "Building radosgw ..."
   echo "CEPH_DIR=${CEPH_DIR}"
   echo "NPROC=${NPROC}"
+  echo "CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
 
   export CCACHE_DIR="${CEPH_DIR}/build.ccache"
   if [ ! -d "${CCACHE_DIR}" ]; then
@@ -40,9 +48,11 @@ build_radosgw() {
 }
 
 strip_radosgw() {
+  [ "${CMAKE_BUILD_TYPE}" == "Debug" -o "${CMAKE_BUILD_TYPE}" == "RelWithDebInfo" ] && return 0
+
   echo "Stripping files ..."
   strip --strip-debug --strip-unneeded \
-    --remove-section=.comment --remove-section=.note \
+    --remove-section=.comment --remove-section=.note.* \
     ${CEPH_DIR}/build/bin/radosgw \
     ${CEPH_DIR}/build/lib/*.so
 }
