696d38e509c340f668eb9e5ce83dc124fa7fd837
Giuseppe Baccini <giuseppe.baccini@suse.com>
2022-06-15 16:11:05 +0200

    Environment enhancements
    - Added partial support to virtualbox provider
    
    Signed-off-by: Giuseppe Baccini <giuseppe.baccini@suse.com>

--- env/setup-vm.sh
+++ env/setup-vm.sh
@@ -3,8 +3,10 @@
 set -e
 
 export BOX_NAME=${BOX_NAME:-"opensuse/Leap-15.3.x86_64"}
+export VM_PROVIDER=${VM_PROVIDER:-"libvirt"}
 export VM_NET=${VM_NET:-"10.46.201.0"}
 export VM_NET_LAST_OCTET_START=${CLUSTER_NET_LAST_OCTET_START:-"101"}
+export VM_BRIDGE_INET=${VM_BRIDGE_INET:-"eth0"}
 export ADMIN_COUNT=${ADMIN_COUNT:-"1"}
 export WORKER_COUNT=${WORKER_COUNT:-"1"}
 export ADMIN_MEM=${ADMIN_MEM:-"4096"}
@@ -17,13 +19,14 @@ export WORKER_DISK=${WORKER_DISK:-"no"}
 export WORKER_DISK_SIZE=${WORKER_DISK_SIZE:-"8G"}
 export CONTAINER_ENGINE=${CONTAINER_ENGINE:-"podman"}
 export STOP_AFTER_BOOTSTRAP=${STOP_AFTER_BOOTSTRAP:-"no"}
+export STOP_AFTER_K3S_INSTALL=${STOP_AFTER_K3S_INSTALL:-"no"}
 export S3GW_IMAGE=${S3GW_IMAGE:-"ghcr.io/aquarist-labs/s3gw:latest"}
 export S3GW_IMAGE_PULL_POLICY=${S3GW_IMAGE_PULL_POLICY:-"Always"}
 export PROV_USER=${PROV_USER:-"vagrant"}
 
 #these defaults will change
 export S3GW_UI_REPO=${S3GW_UI_REPO:-"https://github.com/aquarist-labs/aws-s3-explorer.git"}
-export S3GW_UI_VERSION=${S3GW_UI_VERSION:-"s3gw-demo"}
+export S3GW_UI_VERSION=${S3GW_UI_VERSION:-"s3gw-ui-testing"}
 
 export SCENARIO=${SCENARIO:-"default"}
 export K3S_VERSION=${K3S_VERSION:-"v1.23.6+k3s1"}
@@ -36,8 +39,10 @@ start_env() {
 
 build_env() {
   echo "BOX_NAME=${BOX_NAME}"
+  echo "VM_PROVIDER=${VM_PROVIDER}"
   echo "VM_NET=${VM_NET}"
   echo "VM_NET_LAST_OCTET_START=${VM_NET_LAST_OCTET_START}"
+  echo "VM_BRIDGE_INET=${VM_BRIDGE_INET}"
   echo "ADMIN_COUNT=${ADMIN_COUNT}"
   echo "WORKER_COUNT=${WORKER_COUNT}"
   echo "ADMIN_MEM=${ADMIN_MEM}"
@@ -50,6 +55,7 @@ build_env() {
   echo "WORKER_DISK_SIZE=${WORKER_DISK_SIZE}"
   echo "CONTAINER_ENGINE=${CONTAINER_ENGINE}"
   echo "STOP_AFTER_BOOTSTRAP=${STOP_AFTER_BOOTSTRAP}"
+  echo "STOP_AFTER_K3S_INSTALL=${STOP_AFTER_K3S_INSTALL}"
   echo "S3GW_IMAGE=${S3GW_IMAGE}"
   echo "S3GW_IMAGE_PULL_POLICY=${S3GW_IMAGE_PULL_POLICY}"
   echo "PROV_USER=${PROV_USER}"
