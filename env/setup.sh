s3gw-tech/s3gw

commit a0b41bd14d20b52337582f3820fa1bfd35d66f2c

- Replace s3gw-core by s3gw-tools - Adapt paths that do not exist anymore
Signed-off-by: Volker Theile <vtheile@suse.com>

env/setup.sh
@@ -243,7 +243,7 @@ elif [[ -e "generate-spec.sh" ]]; then
 else
   echo "Installing s3gw..."
   k3s kubectl apply \
-    -f ${ghraw}/aquarist-labs/s3gw-core/main/k3s/s3gw.yaml || (
+    -f ${ghraw}/aquarist-labs/s3gw-tools/env/s3gw.yaml || (
     error "Failed to install s3gw."
     exit 1
   )

build/build-radosgw.sh
@@ -44,7 +44,7 @@ build_radosgw() {
   if [ ! -d "${CCACHE_DIR}" ]; then
     echo "ccache dir not found, create."
     mkdir "${CCACHE_DIR}"
-    echo "Created by aquarist-labs/s3gw-core build-radosgw container" > \
+    echo "Created by aquarist-labs/s3gw-tools build-radosgw container" > \
       "${CCACHE_DIR}/README"
   fi
 

commit 5bb980ddfe800383163a5ac75c8d6c3d376f922a

Reinstate "main" in k3s setup URLs
Fixes: a0b41bd14d20b52337582f3820fa1bfd35d66f2c
Signed-off-by: Tim Serong <tserong@suse.com>

env/setup.sh
@@ -275,7 +275,7 @@ elif [[ -e "generate-spec.sh" ]]; then
 else
   echo "Installing s3gw..."
   k3s kubectl apply \
-    -f ${ghraw}/aquarist-labs/s3gw-tools/env/s3gw.yaml || (
+    -f ${ghraw}/aquarist-labs/s3gw-tools/main/env/s3gw.yaml || (
     error "Failed to install s3gw."
     exit 1
   )