ddda22e57280fcd8b43ef5a4865e90210d869569
Xavi Garcia <xavi.garcia@suse.com>
2023-04-19 15:23:32 +0200

    ci: add s3gw unittests to be executed
    Fixes: https://github.com/aquarist-labs/s3gw/issues/451
    
    Signed-off-by: Xavi Garcia <xavi.garcia@suse.com>

--- tools/build/run_tests.sh
+++ tools/build/run_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 RES=0
 echo "Unit tests found:"
-find /radosgw/bin -name "unittest_rgw_sfs_*"
+find /radosgw/bin -name "unittest_rgw_sfs_*" -o -name "unittest_rgw_s3gw_*"
 
 echo "Running tests..."
-UNIT_TESTS=(`find /radosgw/bin -name "unittest_rgw_sfs_*"`)
+UNIT_TESTS=(`find /radosgw/bin -name "unittest_rgw_sfs_*" -o -name "unittest_rgw_s3gw_*"`)
 for unit_test in "${UNIT_TESTS[@]}"
 do
   ${unit_test}
